c68ad0bbc85518e7295da1705c890427f93dff3c
Faizan Abbasi <abbasi.faizan1997@gmail.com>
2023-12-21 15:17:18 +0500

    Fixed CSRF issue

--- home_services_app/settings.py
+++ home_services_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'home-services.up.railway.app']
 
 # Application definition
 INSTALLED_APPS = [
@@ -131,7 +131,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
   BASE_DIR / "static",
 ]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
