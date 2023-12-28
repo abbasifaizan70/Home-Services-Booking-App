41f1048a0554a16bf7a245dac32e2b5a38e78ec3
Faizan Abbasi <abbasi.faizan1997@gmail.com>
2023-12-28 15:00:22 +0500

    update setupenv.sh file

--- envsetup.sh
+++ envsetup.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-if [ -d "env" ] 
-then
-    echo "Python virtual environment exists." 
+if [ -d "env" ]; then
+    echo "Python virtual environment exists."
 else
-    python3 -m venv env
+    python -m venv env
 fi
 
+# Activate the virtual environment
 source env/bin/activate
 
-
+# Install requirements
 pip3 install -r requirements.txt
 
-if [ -d "logs" ] 
-then
-    echo "Log folder exists." 
+if [ -d "logs" ]; then
+    echo "Log folder exists."
 else
     mkdir logs
     touch logs/error.log logs/access.log
